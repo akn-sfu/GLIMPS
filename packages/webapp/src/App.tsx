56131ad5aac167b7662b38affdbae1be531c6e88
Liyang Zhou <liyang_zhou_2@sfu.ca>
2021-06-17 22:37:36 -0700

    lerna publish

--- packages/webapp/src/App.tsx
+++ packages/webapp/src/App.tsx
@@ -19,7 +19,8 @@ const App = () => {
               <SnackbarProvider
                 maxSnack={3}
                 anchorOrigin={{
-                  vertical: 'bottom', horizontal: 'right',
+                  vertical: 'bottom',
+                  horizontal: 'right',
                 }}
               >
                 <Router />
