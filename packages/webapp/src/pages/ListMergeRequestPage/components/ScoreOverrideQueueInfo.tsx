fb4c55deb7fa908c7e56d88c5be11011f0556d52
Liyang Zhou <liyang_zhou_2@sfu.ca>
2021-07-12 17:40:42 -0700

    remove alert

--- packages/webapp/src/pages/ListMergeRequestPage/components/ScoreOverrideQueueInfo.tsx
+++ packages/webapp/src/pages/ListMergeRequestPage/components/ScoreOverrideQueueInfo.tsx
@@ -7,7 +7,6 @@ import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import Grid from '@material-ui/core/Grid';
-import Alert from '@material-ui/lab/Alert';
 import React, { useState } from 'react';
 import { useUpdateScoreOverrides } from '../../../api/scoring';
 import { useRepositoryContext } from '../../../contexts/RepositoryContext';
@@ -80,68 +79,52 @@ const ScoreOverrideQueueInfo: React.FC = () => {
   return (
     <>
       {queue.length > 0 && (
-        <Box my={2}>
-          <Alert
-            severity='info'
-            action={
-              <Button
-                size='small'
-                color='inherit'
-                onClick={() => setShowDialog(true)}
-              >
-                Review
-              </Button>
-            }
-          >
-            There are pending score override changes.
-          </Alert>
-        </Box>
+        <Dialog
+          open={showDialog}
+          onClose={() => {
+            setShowDialog(false);
+          }}
+          maxWidth='xl'
+        >
+          <DialogTitle>Score Overrides</DialogTitle>
+          <DialogContent style={{ minWidth: '60vw' }}>
+            <Grid container spacing={2}>
+              <Grid item xs={2}>
+                <DialogContentText>
+                  <strong>Current Score</strong>
+                </DialogContentText>
+              </Grid>
+              <Grid item xs={2}>
+                <DialogContentText>
+                  <strong>New Score</strong>
+                </DialogContentText>
+              </Grid>
+              <Grid item xs={8}>
+                <DialogContentText>
+                  <strong>Description</strong>
+                </DialogContentText>
+              </Grid>
+              {queue.map((override) => (
+                <OverrideRow key={override.id} stagedOverride={override} />
+              ))}
+            </Grid>
+          </DialogContent>
+          <DialogActions>
+            <Button
+              color='primary'
+              onClick={() => {
+                reset();
+                setShowDialog(false);
+              }}
+            >
+              Discard
+            </Button>
+            <Button color='primary' variant='contained' onClick={handleUpdate}>
+              Apply Changes
+            </Button>
+          </DialogActions>
+        </Dialog>
       )}
-      <Dialog
-        open={showDialog}
-        onClose={() => {
-          setShowDialog(false);
-        }}
-        maxWidth='xl'
-      >
-        <DialogTitle>Score Overrides</DialogTitle>
-        <DialogContent style={{ minWidth: '60vw' }}>
-          <Grid container spacing={2}>
-            <Grid item xs={2}>
-              <DialogContentText>
-                <strong>Current Score</strong>
-              </DialogContentText>
-            </Grid>
-            <Grid item xs={2}>
-              <DialogContentText>
-                <strong>New Score</strong>
-              </DialogContentText>
-            </Grid>
-            <Grid item xs={8}>
-              <DialogContentText>
-                <strong>Description</strong>
-              </DialogContentText>
-            </Grid>
-            {queue.map((override) => (
-              <OverrideRow key={override.id} stagedOverride={override} />
-            ))}
-          </Grid>
-        </DialogContent>
-        <DialogActions>
-          <Button
-            color='primary'
-            onClick={() => {
-              reset();
-              setShowDialog(false);
-            }}
-          >
-            Discard
-          </Button>
-          <Button color='primary' variant='contained' onClick={handleUpdate}>
-            Apply Changes
-          </Button>
-        </DialogActions>
-      </Dialog>
     </>
   );
 };
