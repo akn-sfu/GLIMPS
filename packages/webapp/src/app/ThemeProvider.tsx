355dd768d7e451c2add0ae254ba40f6a673ae30f
Brian Le <mla283@sfu.ca>
2021-10-03 21:13:25 -0700

    initial sizing attempt

--- packages/webapp/src/app/ThemeProvider.tsx
+++ packages/webapp/src/app/ThemeProvider.tsx
@@ -7,16 +7,16 @@ const theme = createMuiTheme({
   typography: {
     fontFamily: 'Montserrat, sans-serif',
     h1: {
-      fontSize: '3rem',
-    },
-    h2: {
       fontSize: '2.5rem',
     },
-    h3: {
+    h2: {
       fontSize: '2rem',
     },
-    h4: {
+    h3: {
       fontSize: '1.5rem',
+    },
+    h4: {
+      fontSize: '1rem',
     },
   },
 
