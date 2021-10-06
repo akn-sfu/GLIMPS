0a71d4302d050ba7b10f4d86cd1851005f0dce74
mla283 <mla283@sfu.ca>
2021-10-06 12:03:02 -0700

    revert header sizing changes

--- packages/webapp/src/app/ThemeProvider.tsx
+++ packages/webapp/src/app/ThemeProvider.tsx
@@ -7,16 +7,16 @@ const theme = createMuiTheme({
   typography: {
     fontFamily: 'Montserrat, sans-serif',
     h1: {
-      fontSize: '2.5rem',
+      fontSize: '3rem',
     },
     h2: {
-      fontSize: '2rem',
+      fontSize: '2.5rem',
     },
     h3: {
-      fontSize: '1.5rem',
+      fontSize: '2rem',
     },
     h4: {
-      fontSize: '1rem',
+      fontSize: '1.5rem',
     },
   },
 
