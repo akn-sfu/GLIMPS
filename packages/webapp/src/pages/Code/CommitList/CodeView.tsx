c2f41d5bbb7dd4f774cd39c5fa4d10731a94944a
Brian Le <mla283@sfu.ca>
2021-10-25 22:17:26 -0700

    experimental change using xs

--- packages/webapp/src/pages/Code/CommitList/CodeView.tsx
+++ packages/webapp/src/pages/Code/CommitList/CodeView.tsx
@@ -303,17 +303,13 @@ const CodeView: React.FC<CodeViewProps> = ({ mergeRequest, commit }) => {
           alignItems='flex-start'
           spacing={4}
         >
-          <Grid item>
+          <Grid item xs={10}>
             <Typography variant='h2'>
               {commit?.title || mergeRequest?.title}
             </Typography>
           </Grid>
-          <Grid item>
-            {mergeRequest?.iid && (
-              <Grid item>
-                <Typography variant='h6'>{`MR${mergeRequest.iid}`}</Typography>
-              </Grid>
-            )}
+          <Grid item xs={2}>
+            {mergeRequest?.iid && <Grid item>{`MR${mergeRequest.iid}`}</Grid>}
             {allowEdit && (
               <Grid item>
                 <IconButton onClick={onScoreEdit as any}>
