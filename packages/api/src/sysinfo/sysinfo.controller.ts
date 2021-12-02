05104dec09953223854289bf5fc3d6d89a1ce84d
mla283 <mla283@sfu.ca>
2021-12-02 15:29:08 -0800

    implement get size of tables

--- packages/api/src/sysinfo/sysinfo.controller.ts
+++ packages/api/src/sysinfo/sysinfo.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { getManager } from 'typeorm';
 
 @Controller('sysinfo')
-export class SysinfoController {}
+export class SysinfoController {
+  @Get('diskusage')
+  async getDiskUsage() {
+    const manager = getManager();
+    const spaceUsed = await manager.query(`SELECT total_bytes AS total
+    FROM (
+    SELECT *, total_bytes-index_bytes-coalesce(toast_bytes,0) AS table_bytes FROM (
+        SELECT c.oid,nspname AS table_schema, relname AS table_name
+                , c.reltuples AS row_estimate
+                , pg_total_relation_size(c.oid) AS total_bytes
+                , pg_indexes_size(c.oid) AS index_bytes
+                , pg_total_relation_size(reltoastrelid) AS toast_bytes
+            FROM pg_class c
+            LEFT JOIN pg_namespace n ON n.oid = c.relnamespace
+            WHERE relkind = 'r'
+    ) a
+    ) a WHERE "table_schema"='public';`);
+    return { used: spaceUsed };
+  }
+}
