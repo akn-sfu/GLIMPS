d76c461b8fd4d77fd7429513b621a717d129ac82
David Doan <vandoandinh@gmail.com>
2023-05-28 17:19:36 -0700

    Add migration file

---
+++ packages/api/src/migrations/1685319537029-AddRequireSyncToRepository.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class AddRequireSyncToRepository1685319537029 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
